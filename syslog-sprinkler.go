e94990ebff21b33e0f209fd33f6b982fdfcddb4b
jh <code@getbraintree.com>
2015-04-03 20:38:06 +0000

    Rename client and server

--- syslog-sprinkler.go
+++ syslog-sprinkler.go
@@ -1,31 +1,67 @@
 // vim: noexpandtab tabstop=2 shiftwidth=2:
 
-// syslog sprinkler client
+// syslog sprinkler daemon
 package main
 
 import (
 	"fmt"
+	"github.com/jeromer/syslogparser"
+	"github.com/wolfeidau/syslogasuarus/syslogd"
 	"log"
 	"net"
-	"os"
 	"time"
 )
 
-func receiveSyslogs(conn net.Conn) {
+func handleClientHeartbeats(conn *net.UDPConn, clients chan string) {
+
 	var buf [1024]byte
 
 	for {
-		n, err := conn.Read(buf[0:])
-		checkError(err)
-		fmt.Println(string(buf[0:n]))
+		n, addr, err := conn.ReadFromUDP(buf[0:])
+		if err != nil {
+			log.Print("Error: Received bad UDP packet\n")
+		} else if string(buf[0:n]) != "HEARTBEAT" {
+			log.Print("Error: Received packet without a heatbeat message", string(buf[0:n]), "\n")
+		} else {
+			clients <- addr.String()
+		}
 	}
 }
 
-func sendHeartbeats(conn net.Conn) {
+func serveSyslogs(conn *net.UDPConn, clients chan string) {
+	syslogChannel := make(chan syslogparser.LogParts, 1)
+	syslogServer := syslogd.NewServer()
+	syslogServer.ListenUDP(":5514")
+	syslogServer.Start(syslogChannel)
+
+	activeClients := make(map[string]time.Time)
+
 	for {
-		_, err := conn.Write([]byte("HEARTBEAT"))
-		checkError(err)
-		time.Sleep(2 * time.Second)
+		select {
+		case clientAddr := <-clients:
+			activeClients[clientAddr] = time.Now()
+		case logParts := <-syslogChannel:
+			pruneDeadClients(activeClients)
+
+			// Example rsyslog message: 'Apr  3 19:23:40 apply02 nginx-apply: this is the message'
+			unformattedTime := logParts["timestamp"].(time.Time)
+			formattedTime := unformattedTime.Format(time.Stamp)
+			logMessage := fmt.Sprintf("%s %s %s%s %s\n", formattedTime, logParts["hostname"], logParts["tag"], ":", logParts["content"])
+			for clientAddr, _ := range activeClients {
+				udpAddr, _ := net.ResolveUDPAddr("udp4", clientAddr)
+				conn.WriteToUDP([]byte(logMessage), udpAddr)
+			}
+		}
+	}
+}
+
+func pruneDeadClients(activeClients map[string]time.Time) {
+	for clientAddr, lastHeartbeat := range activeClients {
+		now := time.Now()
+		lastHeartbeatDuration := now.Sub(lastHeartbeat)
+		if lastHeartbeatDuration.Seconds() >= 10 {
+			delete(activeClients, clientAddr)
+		}
 	}
 }
 
@@ -36,18 +72,14 @@ func checkError(err error) {
 }
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Fatalf("Usage: %s host:port", os.Args[0])
-	}
-	service := os.Args[1]
-
+	service := ":5515"
 	udpAddr, err := net.ResolveUDPAddr("udp4", service)
 	checkError(err)
 
-	conn, err := net.DialUDP("udp", nil, udpAddr)
+	conn, err := net.ListenUDP("udp", udpAddr)
 	checkError(err)
 
-	go sendHeartbeats(conn)
-
-	receiveSyslogs(conn)
+	clients := make(chan string)
+	go handleClientHeartbeats(conn, clients)
+	serveSyslogs(conn, clients)
 }
