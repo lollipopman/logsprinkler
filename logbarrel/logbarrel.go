10ec0abfdc22728411e749cd642aee1f239ea4d9
lollipopman <jesse.hathaway@getbraintree.com>
2017-05-12 11:41:38 -0500

    working reading from a file

--- logbarrel/logbarrel.go
+++ logbarrel/logbarrel.go
@@ -5,14 +5,24 @@ package main
 
 import (
 	"encoding/json"
-	"os"
 	"flag"
-	"fmt"
-	"log"
+	log "github.com/Sirupsen/logrus"
+	"io"
 	"net"
+	"os"
+	"strconv"
 	"strings"
+	"syscall"
 	"time"
 )
+
+func init() {
+	// Output to stdout instead of the default stderr, could also be a file.
+	log.SetOutput(os.Stderr)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.DebugLevel)
+}
 
 type RemoteHeartbeatMessage struct {
 	Type       string
@@ -20,25 +30,43 @@ type RemoteHeartbeatMessage struct {
 	SyslogTags []string
 }
 
-func receiveSyslogs(conn net.Conn) {
-	readBuffer := make([]byte, 1024)
+func readTag(tag string, stdout chan []byte) {
+	fileName := "./tags/" + tag + "/" + strconv.Itoa(os.Getpid())
+	time.Sleep(1 * time.Second)
+	file, err := os.OpenFile(fileName, os.O_RDONLY|syscall.O_NONBLOCK, os.ModeNamedPipe)
+	checkError(err)
+
 	for {
-		bytesRead, err := conn.Read(readBuffer[0:])
-		checkError(err)
-		fmt.Print(string(readBuffer[0:bytesRead]))
+		readBuffer := make([]byte, 1024)
+		_, err := file.Read(readBuffer[0:])
+		if err != nil {
+			if err != io.EOF {
+				//log.Info(err.Error())
+			}
+		} else {
+			log.Info("BYTES")
+			stdout <- readBuffer
+		}
 	}
 }
 
-func sendHeartbeats(conn net.Conn, syslogTags []string) {
+func printStdout(stdout <-chan []byte) {
+	for bytes := range stdout {
+		os.Stdout.Write(bytes)
+	}
+}
+
+func sendHeartbeats(conn net.Conn, tags []string) {
 	var remoteHeartbeatMessage RemoteHeartbeatMessage
 	remoteHeartbeatMessage.Type = "heartbeat"
-	remoteHeartbeatMessage.SyslogTags = syslogTags
+	remoteHeartbeatMessage.SyslogTags = tags
 	remoteHeartbeatMessage.Pid = os.Getpid()
 	byteHeartbeatMessage, err := json.Marshal(remoteHeartbeatMessage)
 	checkError(err)
 
 	for {
 		_, err := conn.Write(byteHeartbeatMessage)
+		log.Debug("Heartbeat sent")
 		checkError(err)
 		time.Sleep(2 * time.Second)
 	}
@@ -51,16 +79,20 @@ func checkError(err error) {
 }
 
 func main() {
-	var syslogTagsString = flag.String("t", "*", "Comma separated list of Syslog tags or '*' to select all")
-	var syslogTags []string
+	var tagsString = flag.String("t", "*", "Comma separated list of Syslog tags or '*' to select all")
+	var tags []string
 
 	flag.Parse()
-	syslogTags = strings.Split(*syslogTagsString, ",")
+	tags = strings.Split(*tagsString, ",")
 
 	UDPAddr := net.UDPAddr{Port: 5514}
 	conn, err := net.DialUDP("udp", nil, &UDPAddr)
 	checkError(err)
 
-	go sendHeartbeats(conn, syslogTags)
-	receiveSyslogs(conn)
+	go sendHeartbeats(conn, tags)
+	stdout := make(chan []byte)
+	for tag := range tags {
+		go readTag(tags[tag], stdout)
+	}
+	printStdout(stdout)
 }
